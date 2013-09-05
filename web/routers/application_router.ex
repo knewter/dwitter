a606f666f3d22fa041eccff97cedf8d1cc62abca
Josh Adams <josh@isotope11.com>
2013-09-04 23:14:10 -0500

    Add /post path

--- web/routers/application_router.ex
+++ web/routers/application_router.ex
@@ -5,15 +5,20 @@ defmodule ApplicationRouter do
     # Pick which parts of the request you want to fetch
     # You can comment the line below if you don't need
     # any of them or move them to a forwarded router
-    conn.fetch([:cookies, :params])
+    conn = conn.fetch([:cookies, :params])
+    conn = conn.assign(:title, "Welcome to Dwitter!")
   end
 
   # It is common to break your Dynamo into many
   # routers, forwarding the requests between them:
   # forward "/posts", to: PostsRouter
 
+  post "/post" do
+    conn = conn.assign(:content, conn.params[:content])
+    render conn, "post_complete.html"
+  end
+
   get "/" do
-    conn = conn.assign(:title, "Welcome to Dynamo!")
     render conn, "index.html"
   end
 end
