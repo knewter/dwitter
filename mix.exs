eca183a7a8dd9c1a99de17090e56ad5239b15e93
Josh Adams <josh@isotope11.com>
2013-09-13 19:37:12 -0500

    Add bits from Episode 8 (data model with amnesia)

--- mix.exs
+++ mix.exs
@@ -19,6 +19,7 @@ defmodule Dwitter.Mixfile do
 
   defp deps do
     [ { :cowboy, github: "extend/cowboy" },
-      { :dynamo, "0.1.0-dev", github: "elixir-lang/dynamo" } ]
+      { :dynamo, "0.1.0-dev", github: "elixir-lang/dynamo" },
+      { :amnesia, "~> 0.1.0", github: "meh/amnesia"} ]
   end
 end
