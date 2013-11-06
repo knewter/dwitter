75b0668d7db5ae46b74f2e70e49c7414f81dc123
Josh Adams <josh@isotope11.com>
2013-11-06 17:31:05 -0600

    Support Ecto finally

--- web/routers/application_router.ex
+++ web/routers/application_router.ex
@@ -1,6 +1,6 @@
 defmodule ApplicationRouter do
   use Dynamo.Router
-  use Dwitter.Database
+  use Ecto.Query
 
   prepare do
     # Pick which parts of the request you want to fetch
@@ -16,12 +16,16 @@ defmodule ApplicationRouter do
 
   post "/post" do
     # Store a new dweet based on conn.params[:content] and assign to dweet
+    dweet = Dwitter.Dweet.new(content: conn.params[:content], author: "elixirsips")
+    Dwitter.Repo.create(dweet)
     conn = conn.assign(:dweet, dweet)
     render conn, "post_complete.html"
   end
 
   get "/" do
     # Assign recent_dweets to the last 10 dweets
+    query = from d in Dwitter.Dweet, order_by: [desc: d.id], limit: 10, select: d
+    recent_dweets = Dwitter.Repo.all(query)
     conn = conn.assign(:recent_dweets, recent_dweets)
     render conn, "index.html"
   end
